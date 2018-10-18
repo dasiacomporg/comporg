dfd4859cd57ac42059cb903558b45ad5865506cd
dasiacomporg <dasia.scott@bison.howard.edu>
2018-10-18 15:10:18 -0500

    Update project-1.s
    commit 3, trying to print

--- project-1.s
+++ project-1.s
@@ -1,11 +1,8 @@
 #output name and id 
 
-.data
-      
-output_at: .asciiz "@ "
 .text ##assembly language instuctions goes in text segment
 main: #start coding
-li $v0, 4
-la $v0, output_string
-syscall # call operating system to perform operation
+li $v0, 11 #print character system call(like a built-in function)
+la $a0, 64 # load 64 into arg to the system call
+syscall # call OS to print whatever is in $a0 - $a3
 
