60b8085e665d7f9f1cfd1a0b08d42fe9c0879e70
dasiacomporg <dasia.scott@bison.howard.edu>
2018-10-23 13:48:29 -0500

    Update project-1.s
    printed out intergers in my ID number using addition and subtraction functions , and printed them individually

--- project-1.s
+++ project-1.s
@@ -2,7 +2,23 @@
 
 .text ##assembly language instuctions goes in text segment
 main: #start coding
-li $v0, 11 #print character system call(like a built-in function)
+li $v0, 11  #print character system call(like a built-in function)
 la $a0, 64 # load 64 into arg to the system call
 syscall # call OS to print whatever is in $a0 - $a3
-
+sub $v0, 10 #print integers 
+sub $a0, 64 
+syscall #print first number
+add $a0, 2 #50  = 2 on ascii table
+syscall
+add $a0, 6
+syscall
+sub $a0, 7
+syscall
+sub $a0, 0
+syscall
+add $a0, 6
+syscall
+sub $a0, 1
+syscall
+add $a0, 2
+syscall
